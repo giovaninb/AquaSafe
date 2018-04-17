10640fa29838be8bfdb28b848838f3f6ec40baeb
Flavielle <flaviellebm@gmail.com>
2018-04-17 12:07:22 -0300

    Tela Inicial do Checklist e Button pressionado

---
+++ app/src/main/java/com/example/flavielli_marques/aquasafe/ChecklistActivity.java
@@ -0,0 +1,22 @@
+package com.example.flavielli_marques.aquasafe;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ChecklistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checklist);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.checklist);
+    }
+
+}
--- app/src/main/java/com/example/flavielli_marques/aquasafe/MainActivity.java
+++ app/src/main/java/com/example/flavielli_marques/aquasafe/MainActivity.java
@@ -6,9 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import com.example.flavielli_marques.aquasafe.Hazard.HazardActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +16,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageButton hazard_button = (ImageButton) findViewById(R.id.hazard);
+        ImageButton checklist_button = (ImageButton) findViewById(R.id.checklist);
+
         hazard_button.setOnClickListener(click);
+        checklist_button.setOnClickListener(click);
     }
     private View.OnClickListener click = new View.OnClickListener(){
 
@@ -26,10 +29,18 @@ public class MainActivity extends AppCompatActivity {
 
                 case R.id.hazard:
                     String valorTag = v.getTag().toString();
-                    Toast.makeText(getBaseContext(), valorTag, Toast.LENGTH_SHORT).show();
+                  //  Toast.makeText(getBaseContext(), valorTag, Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(MainActivity.this, HazardActivity.class);
                     startActivity(intent);
                     break;
+
+                case R.id.checklist:
+                    valorTag = v.getTag().toString();
+                 //   Toast.makeText(getBaseContext(), valorTag, Toast.LENGTH_SHORT).show();
+                    intent = new Intent(MainActivity.this, ChecklistActivity.class);
+                    startActivity(intent);
+                    break;
+
             }
         }
     };
