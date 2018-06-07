7817ce9a837a4df36211b35e2f4563848cf774d4
Flavielle <flaviellebm@gmail.com>
2018-06-07 16:05:29 -0300

    Cores do App atualizadas,imagens da tela sinalização também.

--- app/src/main/java/com/example/flavielli_marques/aquasafe/Sinalization/SinalizationActivity.java
+++ app/src/main/java/com/example/flavielli_marques/aquasafe/Sinalization/SinalizationActivity.java
@@ -1,12 +1,18 @@
 package com.example.flavielli_marques.aquasafe.Sinalization;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.flavielli_marques.aquasafe.R;
 
+import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
+
 public class SinalizationActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +20,6 @@ public class SinalizationActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.signalization);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
+
     }
 }
